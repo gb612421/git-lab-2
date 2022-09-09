4cd92056ac0370a29a1008625b10ff839060e250
gb612421 <gb612421@ohio.edu>
2022-09-09 17:39:34 -0400

    Final

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Garrett Bryars
+ *        Date: 9/9/2022
+ * Description: git-lab-2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
